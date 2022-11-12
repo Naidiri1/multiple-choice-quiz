6385af025b8cb20a078e6f7d5d86d426af0b3b4a
Iris <iridian95@live.com>
2022-11-12 18:40:06 -0500

    adding questions to js file

---
+++ assets/js/questions.js
@@ -0,0 +1,35 @@
+// List of all questions, choices, and answers
+
+//Question #1 
+var questions = [
+{
+    title: "Which type of JavaScript language is ___ .",
+    choices: ["A. Object-Oriented", "B. Object-Based", "C. Assembly-language", "D. Assembly-language"],
+    answer: "B. Object-Based "
+},
+//Question #2 
+{
+    title: "Which one of the following is also known as Conditional Expression",
+    choices: ["A. Alternative to if-else", "B. Switch statement", "C. If-then-else statement", "D. immediate if"],
+    answer: "D. immediate if",
+},
+//Question #3 
+{   
+    tittle: "The function and  var are known as: ",
+    choices: ["A. Keywords", "B. Data types", "C. Declaration statements", "D. Prototypes"],
+    answer: "C.Declaration statements",
+
+},
+//Question #4
+{
+  tittle: "Which of the following variables takes precedence over the others if the names are the same",
+  choices: ["A. Global variable", "B. The local element", "C. The two of the above", "D. None of the above"],
+  answer: "B. The local element",
+},
+//Question #5
+{
+  title: "In the JavaScript, which one of the following is not considered as an error:",
+  choices: ["A. Syntax error", "B. Missing of semicolons", "C. Division by zero", "D. Missing of Bracket"],
+  answer: "C. Division by zero",
+},
+];
